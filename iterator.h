09451a81e2c59bd226b31350dd6fb53e2e86349e
Marvin <652141046@qq.com>
2017-11-16 17:58:17 +0800

    iterator

---
+++ iterator.h
@@ -0,0 +1,76 @@
+#ifndef MYSTL_ITERATOR_H_
+#define MYSTL_ITERATOR_H_
+
+#include <cstddef> //for ptrdiff_t
+namespace mystl {
+
+struct input_iterotor_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterotor_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag :public bidirectional_iterator_tag {};
+
+template<typename Category, typename T, typename Distance = ptrdiff_t,
+        typename Pointer = T*, typename Reference = T&>
+struct iterator {
+    typedef Category    iterator_category;
+    typedef T           value_type;
+    typedef Distance    difference_type;
+    typedef Pointer     pointer;
+    typedef Reference   reference;
+};
+
+//traits
+template<typename Iterator>
+struct iterator_traits {
+    typedef typename Iterator::iterator_category iterator_category;
+    typedef typename Iterator::value_type        value_type;
+    typedef typename Iterator::difference_type   difference_type;
+    typedef typename Iterator::Pointer           pointer;
+    typedef typename Iterator::reference         reference;
+};
+
+//针对原生指针而设计的traits特化版
+template<typename T>
+struct iterator_traits<T*> {
+    typedef random_access_iterator_tag iterator_category;
+    typedef T                          value_type;
+    typedef ptrdiff_t                  difference_type;
+    typedef T*                         pointer;
+    typedef T&                         reference;
+};
+
+template<typename T>
+struct iterator_traits<const T*> {
+    typedef random_access_iterator_tag iterator_category;
+    typedef T                          value_type;
+    typedef ptrdiff_t                  difference_type;
+    typedef const T*                   pointer;
+    typedef const T&                   reference;
+};
+
+//返回迭代器类型的函数
+template<typename Iterator>
+inline typename iterator_traits<Iterator>::iterator_category
+iterator_category(const Iterator&) {
+    typedef typename iterator_traits<Iterator>::iterator_category category;
+    return category();
+}
+
+//返回distance type的函数
+template <typename Iterator>
+inline typename iterator_traits<Iterator>::difference_type*
+distance_type(const Iterator&) {
+    return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+}
+
+//返回value type的函数
+template<typename Iterator>
+inline typename iterator_traits<Iterator>::value_type*
+value_type(const Iterator&) {
+    return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+}
+
+} //namespace mystl
+
+#endif
