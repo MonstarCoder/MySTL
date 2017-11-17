1433856748b382a88ad514242c7007fe0ae7bd10
Marvin <652141046@qq.com>
2017-11-17 17:37:05 +0800

    vector

---
+++ vector.h
@@ -0,0 +1,17 @@
+#ifndef MYSTL_VTCTOR_H
+#define MYSTL_VTCTOR_H
+
+#include "allocator.h"
+#include "typetraits.h"
+#include "iterator.h"
+
+#include <algorithm> //for fill_n
+#include <memory> //for uninitialized_copy unititialized_fill
+
+namespace mystl {
+
+
+
+} //namespace mystl
+
+#endif
