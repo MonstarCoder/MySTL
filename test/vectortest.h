bf6fd44fe0f58f1f17d760893eb1b261d3404d78
Marvin <652141046@qq.com>
2017-11-22 10:57:42 +0800

    重命名

---
+++ test/vectortest.h
@@ -0,0 +1,43 @@
+#ifndef MYSTL_VECTOR_TEST_H_
+#define MYSTL_VECTOR_TEST_H_
+
+#include "../vector.h"
+#include "testutil.h"
+
+#include <vector>
+
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+namespace mystl{
+namespace vectortest{
+    template<class T>
+    using stdVec = std::vector<T>;
+
+    template<class T>
+    using tsVec = mystl::vector<T>;
+
+    void testCase1();
+    void testCase2();
+    void testCase3();
+    void testCase4();
+    void testCase5();
+    void testCase6();
+    void testCase7();
+    void testCase8();
+    void testCase9();
+    void testCase10();
+    void testCase11();
+    void testCase12();
+    void testCase13();
+    void testCase14();
+
+    void testAllCases();
+
+} //namespace vectortest
+} //namespace mystl
+
+#endif
