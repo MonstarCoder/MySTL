ade446411f0efa29361db2d3f6cd2d0f4d3c27ad
Marvin <652141046@qq.com>
2017-11-26 17:16:25 +0800

    增加系统资源用量测试：getrusage()

--- profiler/profiler.cc
+++ profiler/profiler.cc
@@ -5,6 +5,7 @@ namespace profiler {
 
 struct timeval ProfilerInstance::start_;
 struct timeval ProfilerInstance::finish_;
+struct rusage ProfilerInstance::usage;
 
 void ProfilerInstance::start() {
     gettimeofday(&start_, NULL);
@@ -19,6 +20,11 @@ void ProfilerInstance::print_time() {
     timer = 1000000 * (finish_.tv_sec - start_.tv_sec) + finish_.tv_usec - start_.tv_usec;
     timer /= 1000;
     std:: cout << "used time: " << timer << "ms" << std::endl;
+}
+
+size_t ProfilerInstance::memory() {
+    getrusage(RUSAGE_SELF, &usage);
+    return usage.ru_maxrss; //kb
 }
 
 } //namespace profiler
--- profiler/profiler.h
+++ profiler/profiler.h
@@ -3,6 +3,7 @@
 
 #include <sys/time.h> //for gettimeofday()
 #include <sys/resource.h> //for getrusage()
+#include <stddef.h> //for size_t
 
 #include <iostream>
 
@@ -14,9 +15,11 @@ public:
     static void start(); //开始计时
     static void finish(); //结束计时
     static void print_time(); //打印用时
+    static size_t memory(); //查询当前程序的内存使用量
 private:
-    static struct timeval start_; //系统结构
-    static struct timeval finish_; //系统结构
+    static struct timeval start_; //for gettimeofday
+    static struct timeval finish_; //for gettimeofday
+    static struct rusage usage; //for getrusage
 };
 
 
