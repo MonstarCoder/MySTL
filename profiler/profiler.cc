840f502b62d48bd22df62d60d7bb008610252e99
Marvin <652141046@qq.com>
2017-11-26 16:56:56 +0800

    bug fix: 时间单位修复

--- profiler/profiler.cc
+++ profiler/profiler.cc
@@ -17,6 +17,7 @@ void ProfilerInstance::finish() {
 void ProfilerInstance::print_time() {
     unsigned long timer;
     timer = 1000000 * (finish_.tv_sec - start_.tv_sec) + finish_.tv_usec - start_.tv_usec;
+    timer /= 1000;
     std:: cout << "used time: " << timer << "ms" << std::endl;
 }
 
