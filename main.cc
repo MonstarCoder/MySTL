0ff3f64d4395b87d8d3ae2d89cfd37114968995b
Marvin <652141046@qq.com>
2017-11-29 21:31:51 +0800

    增加list测试

--- main.cc
+++ main.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 
 #include "./test/vectortest.h"
+#include "./test/listtest.h"
 
 using namespace mystl;
 
 int main() {
 
 	mystl::vectortest::testAllCases();
+	mystl::listtest::testAllCases();
 
 	return 0;
 }
