269c3a8a8570e26d604c0a0e9229b95ebcb845e3
Marvin <652141046@qq.com>
2017-12-02 15:40:54 +0800

    bug fix: input_iterator_tag

--- iterator.h
+++ iterator.h
@@ -6,7 +6,7 @@ namespace mystl {
 
 struct input_iterator_tag {};
 struct output_iterator_tag {};
-struct forward_iterator_tag : public input_iterotor_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag :public bidirectional_iterator_tag {};
 
